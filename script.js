d5e45d2af17b7e667a36a03f3f19bb075b4aebbe
zantimer <karol.nikiel1@gmail.com>
2022-11-04 10:59:15 +0000

    basic structure

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(a,b)
+{
+    return a+b;
+};
+
+function substract(a,b)
+{
+    return a-b;
+};
+
+function multiply(a,b)
+{
+    return a*b;
+};
+
+function divide(a,b)
+{
+    if(a!=0 && b!=0)
+    {
+    return a/b;
+    }
+    else
+    {
+        return 'get outta here';
+    }
+};
