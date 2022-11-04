e46f39f8dde7f260e4c578ec0499d829c2ac7b6d
zantimer <karol.nikiel1@gmail.com>
2022-11-04 13:08:21 +0000

    skeleton of equation logic, needs refactor

--- script.js
+++ script.js
@@ -1,3 +1,98 @@
+//button queries
+const btn0 = document.querySelector('.zero');
+const btn1 = document.querySelector('.one');
+const btn2 = document.querySelector('.two');
+const btn3 = document.querySelector('.three');
+const btn4 = document.querySelector('.four');
+const btn5 = document.querySelector('.five');
+const btn6 = document.querySelector('.six');
+const btn7 = document.querySelector('.seven');
+const btn8 = document.querySelector('.eight');
+const btn9 = document.querySelector('.nine');
+
+const btnSum = document.querySelector('.add');
+const btnMinus = document.querySelector('.minus');
+const btnTimes = document.querySelector('.times');
+const btnDiv = document.querySelector('.divide');
+const btnEquals = document.querySelector('.equals');
+const btnClear = document.querySelector('.clear');
+
+//display logic
+const displayText = document.querySelector('.displayText');
+btn0.addEventListener('click', () => displayText.textContent += 0);
+btn1.addEventListener('click', () => displayText.textContent += 1);
+btn2.addEventListener('click', () => displayText.textContent += 2);
+btn3.addEventListener('click', () => displayText.textContent += 3);
+btn4.addEventListener('click', () => displayText.textContent += 4);
+btn5.addEventListener('click', () => displayText.textContent += 5);
+btn6.addEventListener('click', () => displayText.textContent += 6);
+btn7.addEventListener('click', () => displayText.textContent += 7);
+btn8.addEventListener('click', () => displayText.textContent += 8);
+btn9.addEventListener('click', () => displayText.textContent += 9);
+//sum
+btnSum.addEventListener('click', ()=>displayText.textContent += ' + ');
+
+//minus
+btnMinus.addEventListener('click', ()=>displayText.textContent += ' - ');
+
+//times
+btnTimes.addEventListener('click', ()=>displayText.textContent += ' * ');
+
+//div
+btnDiv.addEventListener('click', ()=>displayText.textContent += ' / ');
+
+btnClear.addEventListener('click', ()=>displayText.textContent = '');
+//main calculation
+btnEquals.addEventListener('click', ()=>{
+    
+    const equation = displayText.textContent.split(' ');
+    console.log(equation);
+    let result = 0;
+    let newEquationLeft;
+    let newEquationRight;
+    if (equation.includes('*'))
+    {
+    for (let i=0;i<equation.length;i++)
+    {
+        if(equation[i] == '*')
+        {
+            result += multiply(equation[i-1], equation[i+1])
+            if (equation.length > 3 
+                && equation[i-1] != equation[0] 
+                && equation[i+1] != equation[equation.length-1])
+            {
+                equation[i-1] = result;
+                equation[i+1] = result;
+            }
+            else if(equation.length>3
+                && equation[i-1]==equation[0])
+                {
+                    equation[i+1]=result;
+                }
+            else if(equation.length>3
+                && equation[i-1]!=0
+                && equation[i+1]==equation[equation.length-1])
+                {
+                    equation[i-1] = result;
+                }
+        }
+    }
+    }
+    if (equation.includes('/'))
+    {
+    for (let i=0;i<equation.length;i++)
+    {
+        if(equation[i] == '/')
+        {
+            result = divide(equation[i-1], equation[i+1])
+        }
+    }
+    }
+    displayText.textContent = result;
+})
+
+
+// basic operation logic
 function add(a,b)
 {
     return a+b;
@@ -24,3 +119,20 @@ function divide(a,b)
         return 'get outta here';
     }
 };
+
+function operate(numA, numB, operator)
+{
+    if(operator == '+')
+    {
+    return numA + `${operator}` + numB +'='+add(numA, numB);
+    }
+    else if(operator=='-')
+    return numA + `${operator}` + 
+            numB +'='+substract(numA, numB);
+    else if(operator=='*')
+    return numA + `${operator}` + 
+            numB +'='+multiply(numA, numB);
+    else if(operator=='/')
+    return numA + `${operator}` + 
+            numB +'='+divide(numA, numB);
+}
